5bcdaa1b732474d1649ba58acbd1319292c8d6f5
amirgal <amirc538@gmail.com>
2020-02-23 20:30:07 +0200

    added handlebars scripts

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -4,39 +4,31 @@ class Renderer {
         this.addTripTemplate = Handlebars.compile($("#add-trip-template").html())
         this.mainTripTemplate = Handlebars.compile($("#main-trip-template").html())
         this.mainSpotTemplate = Handlebars.compile($("#main-spot-template").html())
+        this.newSpotTemplate = Handlebars.compile('#new-spot-template').html()
     }
     
     renderMyTrips(trips) {
-        $('#side-header').empty().append('<h3>My Trips</h3>')
         const newHTML = this.allTripsTemplate({trip: trips});
-        $('#side-container').empty().append(newHTML);
-        $('#side-footer').empty().append('<button id="newTripBtn">Start New Trip</button>')
+        $('#side-bar').empty().append(newHTML);
     }
 
     renderNewTrip() {
-        $('#side-header').empty().append('<h3>Create New Trip</h3>')
         const newHTML = this.addTripTemplate();
-        $('#side-container').empty().append(newHTML);
-        $('#side-footer').empty().append('<button id="backToTripsBtn">Go Back</button><button id="saveTripBtn">Create Trip</button>')
+        $('#side-bar').empty().append(newHTML);
     }
 
     renderTrip(trip) {
-        $('#side-header').empty().append(`<h3>My ${trip.name}</h3>`)
         const newHTML = this.mainTripTemplate({spot: trip.spots});
-        $('#side-container').empty().append(newHTML);
-        $('#side-footer').empty().append('<button id="backToTripsBtn">Go Back</button><button id="addSpotBtn">Add New Spot</button>')
+        $('#side-bar').empty().append(newHTML);
     }
 
     renderSpot(spot) {
-        $('#side-header').empty().append(`<h3>My ${spot.name}</h3>`)
-        const newHTML = this.mainSpotTemplate();
-        $('#side-container').empty().append(newHTML);
-
+        const newHTML = this.mainSpotTemplate({spot});
+        $('#side-bar').empty().append(newHTML);
     }
 
-    renderNewSpot() {
-        $('#side-header').empty().append('<h3>Create New Spot</h3>')
-        const newHTML = this.mainSpotTemplate();
-        $('#side-container').empty().append(newHTML);
+    renderNewSpot(trip) {
+        const newHTML = this.newSpotTemplate({trip});
+        $('#side-bar').empty().append(newHTML);
     }
 }
