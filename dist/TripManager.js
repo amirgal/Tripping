bc4aae7c22edc5967d4682e93994b9521329a3d1
Alon Meiri <alon@Alons-MacBook-Pro.local>
2020-02-23 17:29:43 +0200

    added update route, fixed coords bug

--- dist/TripManager.js
+++ dist/TripManager.js
@@ -5,15 +5,11 @@ class TripManager {
 
   async getTrips() {
     this.myTrips = await $.get("/myTrips");
-    console.log(this.myTrips);
-    
   }
 
   async saveTrip(trip) {
     this.myTrips.push(trip);
     await $.post("/trip", trip);
-    console.log(this.myTrips);
-    
   }
 
   async saveSpot(spot) {
@@ -27,39 +23,32 @@ class TripManager {
     const spotIndex = this.myTrips[index].spots.findIndex(
       s => s.name == spot.name
     );
-    $.ajax({
+    await $.ajax({
       url: `/spot/${spot._id}`,
-      type: "DELETE",
-      success: function() {
-        this.myTrips[index].spots.splice(spotIndex, 1);
-      }
+      type: "DELETE"
     });
+    this.myTrips[index].spots.splice(spotIndex, 1);
   }
 
-  deleteTrip(trip){
-      const index = this.myTrips.findIndex(t => t.name == trip.name)
-      $.ajax({
-        url: `/trip/${trip.name}`,
-        type: "DELETE",
-        success: function() {
-          this.myTrips.splice(index,1)
-        }
-      });
+  async deleteTrip(trip) {
+    const index = this.myTrips.findIndex(t => t.name == trip.name);
+    await $.ajax({
+      url: `/trip/${trip.name}`,
+      type: "DELETE"
+    });
+    this.myTrips.splice(index, 1);
   }
-  updateSpot(spot){
+
+  async updateSpot(spot) {
     const index = this.myTrips.findIndex(t => t.name == spot.trip);
     const spotIndex = this.myTrips[index].spots.findIndex(
       s => s.name == spot.name
     );
-    $.ajax({
-        url: `/spot`,
-        type: "PUT",
-        data: spot,
-        success: function(res) {
-            this.myTrips[index].spots.splice(spotIndex,1,spot)
-        }
-      });
+    await $.ajax({
+      url: `/spot`,
+      type: "PUT",
+      data: spot
+    });
+    this.myTrips[index].spots.splice(spotIndex, 1, spot);
   }
 }
-
-
--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,26 @@
 // const mapManager = new MapManager
 // const renderer = new Renderer
-// const tripManager = new tripManager
+const tripManager = new TripManager()
 
 
-async const loadPage = function() {
-    initMap()
+const loadPage = async function() {
+    // initMap()
     await tripManager.getTrips()
-    renderer.renderMyTrips(tripManager.myTrips)
+    // renderer.renderMyTrips(tripManager.myTrips)
+    // await tripManager.getTrips()
+    // let t1 = new Trip("alon",new Date())
+    // await tripManager.saveTrip(t1)
+    // let s1 = new Spot("dsdsd","alon", {lat:3, lng:6},"dskjdskjdsa")
+    // await tripManager.saveSpot(s1)
+    const updated = tripManager.myTrips[0].spots[0]
+    updated.coords = {lat:38.00,lng:33.000}
+    console.log(updated);
+    
+    await tripManager.updateSpot(updated)
+
 }
 
-
+loadPage()
 
 // //display trip on trip click
 // $('#side-container').on('click','.tripBtn', function() {
